558589ff1a686504e6979fcad6d5c85364dd711b
skulia15 <skuliarnarsson@gmail.com>
2017-09-26 16:34:26 +0000

    Able to find request mode and updated plan

--- src/httpd.c
+++ src/httpd.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <stdbool.h>
 #include <glib.h>
 #include <sys/time.h>
 #include <errno.h>
@@ -48,7 +49,8 @@ int main(int argc, char *argv[])
     memset(&server, 0, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = htons(portNo);
+	server.sin_port = htons(portNo);
+	// Bind to socket
 	if(bind(serverFd, (struct sockaddr *)&server, (socklen_t)sizeof(server)) < 0){
 		printf("Failed to bind the socket. %s\n", strerror(errno));
 		close(serverFd);
@@ -68,6 +70,7 @@ int main(int argc, char *argv[])
 		timeout.tv_sec = 30;
 		
 		socklen_t len = (socklen_t)sizeof(client);
+		// Accept connection
 		clientFd = accept(serverFd, (struct sockaddr *) &client, &len);
 		if(clientFd < 0){
 			printf("Failed to accept connection. %s\n", strerror(errno));
@@ -86,7 +89,44 @@ int main(int argc, char *argv[])
 		message[n] = '\0';
 		//Remove this!
 		fprintf(stdout, "Received:\n%s\n", message);
+
+		// Count the number of lines in the string
+		// Important! https://stackoverflow.com/questions/9052490/find-the-count-of-substring-in-string
 		
+		const char *tmp = message;
+		int countLines = 0;
+		// "\r\n" is a newline character 
+		while((tmp = (strstr(tmp, "\r\n")))){
+			countLines++;
+			tmp++;
+		} 
+		fprintf(stdout, "Number of lines is: %d\n", countLines);
+
+		// Split the string by newline and put them in an array
+		gchar **splitString = g_strsplit(message, "\r\n", countLines);
+		gchar** dataField;
+		gchar** value;
+		int i;
+		for(i = 0; i < countLines; i++){
+			fprintf(stdout, "Line %d: %s\n", i, splitString[i]);
+			// Split each line into dataField and value
+			dataField = g_strsplit(splitString[i], ": ", sizeof(splitString[i]));
+			fprintf(stdout, "Data Field: %s \n", dataField[0]);
+			// Do operation depending on the mode
+			if(strstr(dataField[0], "GET") != NULL){
+				fprintf(stdout, "GET == %s \n",  dataField[0]);
+				// Do get
+			}
+			else if(strstr(dataField[0], "POST") != NULL){
+				fprintf(stdout, "POST == %s \n", dataField[0]);
+				// Do Post
+			}
+			else if(strstr(dataField[0], "HEAD") != NULL){
+				fprintf(stdout, "HEAD == %s \n", dataField[0]);
+				// Do Head
+			}
+
+		}
 		size_t size = strlen("Hey, Here is your data\n");
 		send(clientFd, "Hey, Here is your data\n", size, 0);
 		
@@ -103,17 +143,24 @@ int main(int argc, char *argv[])
 
 		Start server with: ./httpd 59442
 		Test server in seperate teminal with command: curl -X (GET/POST/HEAD) localhost:59442
+		Test with data: curl -H "Content-Type: application/json" -X POST -d '{"username": "KYS"}' localhost:59442
 
-		validate args
-		Find port number from args
-		Connect to client
-		Bind a TCP socket
-		Receive message request/response? from client
+		validate args X
+		Find port number from args X
+		Connect to client X
+		Connect to multiple clients in parallel
+		Bind a TCP socket X
+		Receive message request from client X
+		Check cookie
 		Parse the header
-			check if persistent
+		Check for persistence
+			if HTTP/1.0 -> not persistent
+		Check Operation (GET / POST/ HEAD)
 		Make Get
 		Make Post
 		Make Head
+		Send Response
+		Send cookie
 		Free Memory
 		Close the connection
 
