d4f6229b4ac185ff7442996e46f745867a4e363d
Skuli Arnarsson <skulia15@ru.is>
2017-09-27 15:37:48 +0000

    Header construction

--- src/httpd.c
+++ src/httpd.c
@@ -11,22 +11,62 @@
 #include <stdbool.h>
 #include <glib.h>
 #include <sys/time.h>
+#include <time.h>
 #include <errno.h>
 
 #define MESSAGE_SIZE 512
 #define MAX_CLIENTS 20
 
+// TODO 
+/* PSEUDOCODE PLANNING
+	Our port is: 59442
+	RFC for Http 1.1: https://tools.ietf.org/html/rfc2616
+
+	Start server with: ./httpd 59442
+	Test server in seperate teminal with command: curl -X (GET/POST/HEAD) localhost:59442
+	Test with data: curl -H "Content-Type: application/json" -X POST -d '{"username": "KYS"}' localhost:59442
+	scp /home/skulipardus/Háskólinn\ í\ Reykjavík/Tölvusamskipti/Http-Server-pt1/src/httpd.c  skulia15@skel.ru.is:tsam/pa2/src/httpd.c
+
+	validate args X
+	Find port number from args X
+	Connect to client X
+	Connect to multiple clients in parallel
+	Bind a TCP socket X
+	Receive message request from client X
+	Check cookie
+	Parse the header
+	Check for persistence
+		if HTTP/1.0 -> not persistent
+	Check Operation (GET / POST/ HEAD)
+	Make Get
+	Make Post
+	Make Head
+	Send Response
+	Send cookie
+	Free Memory
+	Close the connection
+
+	For each request print a single line to a log file that conforms to the format:
+		timestamp : <client ip>:<client port> <request method>
+		<requested URL> : <response code>
+
+
+*/
 /* Function definitions go here */
+void doMethod(int clientFd, gchar* methodType, gchar* protocol);
+void doGet(int clientFd);
+void doPost(int clientFd);
+void doHead(int clientFd, gchar* protocol);
 
 int main(int argc, char *argv[])
 {
 	int serverFd;
 	int clientFd;
 	int portNo;
-	int isPersistent = TRUE;
+	//int isPersistent = TRUE;
 	struct sockaddr_in server, client;
 	char message[MESSAGE_SIZE];
-
+	
 	// Get the port number given in arguments
 	// The number of arguments should be 1 or more?
 	if(argc < 2){
@@ -36,7 +76,8 @@ int main(int argc, char *argv[])
 	else{
 		portNo = atoi(argv[1]);
 	}
-
+	
+	
 	// Create and bind a TCP socket
 	serverFd = socket(AF_INET, SOCK_STREAM, 0);
 	if(serverFd < 0){
@@ -56,7 +97,10 @@ int main(int argc, char *argv[])
 		close(serverFd);
 		return -1;
 	}	
-
+	// initialize timeout
+	//struct timeval timeout;
+	//timeout.tv_sec = 30;
+	
 	// Listen for connections, max 128 connections (128 because it's good practice?)
 	if(listen(serverFd, 128) == -1){
 		printf("Failed listening for connections. %s\n", strerror(errno));
@@ -65,17 +109,13 @@ int main(int argc, char *argv[])
 	}
 
 	for (;;) {
-		// initialize timeout
-		struct timeval timeout;
-		timeout.tv_sec = 30;
-		
 		socklen_t len = (socklen_t)sizeof(client);
 		// Accept connection
 		clientFd = accept(serverFd, (struct sockaddr *) &client, &len);
 		if(clientFd < 0){
 			printf("Failed to accept connection. %s\n", strerror(errno));
 		}
-		fprintf(stdout, "Client has connected\n");
+		fprintf(stdout, "Client has connected\n\n");
 
 		ssize_t n = recvfrom(clientFd, message, sizeof(message) - 1, 0, (struct sockaddr *)&client, &len);
 		// Failed to receive from socket
@@ -92,7 +132,6 @@ int main(int argc, char *argv[])
 
 		// Count the number of lines in the string
 		// Important! https://stackoverflow.com/questions/9052490/find-the-count-of-substring-in-string
-		
 		const char *tmp = message;
 		int countLines = 0;
 		// "\r\n" is a newline character 
@@ -103,34 +142,27 @@ int main(int argc, char *argv[])
 		fprintf(stdout, "Number of lines is: %d\n", countLines);
 
 		// Split the string by newline and put them in an array
-		gchar **splitString = g_strsplit(message, "\r\n", countLines);
-		gchar** dataField;
-		gchar** value;
-		int i;
-		for(i = 0; i < countLines; i++){
-			fprintf(stdout, "Line %d: %s\n", i, splitString[i]);
-			// Split each line into dataField and value
-			dataField = g_strsplit(splitString[i], ": ", sizeof(splitString[i]));
-			fprintf(stdout, "Data Field: %s \n", dataField[0]);
-			// Do operation depending on the mode
-			// Check 
-			if(strstr(dataField[0], "GET") != NULL){
-				fprintf(stdout, "GET == %s \n",  dataField[0]);
-				// Do get
-			}
-			else if(strstr(dataField[0], "POST") != NULL){
-				fprintf(stdout, "POST == %s \n", dataField[0]);
-				// Do Post
-			}
-			else if(strstr(dataField[0], "HEAD") != NULL){
-				fprintf(stdout, "HEAD == %s \n", dataField[0]);
-				// Do Head
-			}
+		gchar**splitString = g_strsplit(message, "\r\n", countLines);
+		gchar** firstLine = g_strsplit(splitString[0], " / ", sizeof(splitString[0]));
+		// Find the method type (GET/POST/HEAD)
+		gchar* methodType = firstLine[0];
+		// Find the protocol (HTTP.1.1) 
+		gchar* protocol = firstLine[1];
+		fprintf(stdout, "Method Type:: %s \n", methodType);
+		fprintf(stdout, "Protocol: %s \n", protocol);
+		// Find the value of the headers of the request
+		gchar** headerFields;
+		int i = 1;
+		for(i; i < countLines; i++){
+			//fprintf(stdout, "Line %d: %s\n", i, splitString[i]);
+			// Split each line into headerFields and value
+			headerFields = g_strsplit(splitString[i], ": ", sizeof(splitString[i]));
+			fprintf(stdout, "Data Field: %s \n", headerFields[0]);
+			fprintf(stdout, "Value Field: %s \n", headerFields[1]);
 		}
+		fprintf(stdout, "Protocol is == %s \n", methodType);
 
-		// Send data back to client
-		size_t size = strlen("Hey, Here is your data\n");
-		send(clientFd, "Hey, Here is your data\n", size, 0);
+		doMethod(clientFd, methodType, protocol);
 
 		// Check for persistence
 		/*
@@ -147,44 +179,72 @@ int main(int argc, char *argv[])
 		close(clientFd);
 		close(serverFd);
 
-	// Listen on multiple sockets
-	// TODO 
 
-	/* PSEUDOCODE PLANNING
-		Our port is: 59442
-		RFC for Http 1.1: https://tools.ietf.org/html/rfc2616
-
-		Start server with: ./httpd 59442
-		Test server in seperate teminal with command: curl -X (GET/POST/HEAD) localhost:59442
-		Test with data: curl -H "Content-Type: application/json" -X POST -d '{"username": "KYS"}' localhost:59442
-
-		validate args X
-		Find port number from args X
-		Connect to client X
-		Connect to multiple clients in parallel
-		Bind a TCP socket X
-		Receive message request from client X
-		Check cookie
-		Parse the header
-		Check for persistence
-			if HTTP/1.0 -> not persistent
-		Check Operation (GET / POST/ HEAD)
-		Make Get
-		Make Post
-		Make Head
-		Send Response
-		Send cookie
-		Free Memory
-		Close the connection
-
-		For each request print a single line to a log file that conforms to the format:
-			timestamp : <client ip>:<client port> <request method>
-			<requested URL> : <response code>
-
-
-	*/
 	return 0;
 }
+void doMethod(int clientFd, gchar* methodType, gchar* protocol){
+	// Do method depending on the mode
+		if( g_strcmp0(methodType, "GET") == 0){
+			// Do get
+			doGet(clientFd);
+		}
+		else if(g_strcmp0(methodType, "POST") == 0){
+			// Do Post
+			doPost(clientFd);
+		}
+		else if(g_strcmp0(methodType, "HEAD") == 0){
+			// Do Head
+			doHead(clientFd, protocol);
+		}
+}
+
+void doGet(int clientFd){
+	// Send data back to client
+	size_t size = strlen("This is a Web page, status code is 200?\n");
+	send(clientFd, "This is a Web page, status code is 200?\n", size, 0);
+
+}
+void doPost(int clientFd){
+	// Send data back to client
+	size_t size = strlen("Post Received\n");
+	send(clientFd, "Post Received\n", size, 0);
+	
+}
+void doHead(int clientFd, gchar* protocol){
+	// https://stackoverflow.com/questions/1442116/how-to-get-date-and-time-value-in-c-program/30759067#30759067
+	// Configure time to the current time
+	time_t t = time(NULL);
+	struct tm *tm = localtime(&t);
+	char timeStamp[64];
+	strftime(timeStamp, sizeof(timeStamp), "%c", tm);
+
+	// Create the header
+	char header[2048];
+	strcat(header, protocol);
+	strcat(header, " 200 OK\n");
+	strcat(header, "Date: ");
+	strcat(header, timeStamp);
+	strcat(header, "\n");
+	strcat(header, "Server: ");
+	strcat(header, "ME\n");
+	strcat(header, "Content-Length: ");
+	strcat(header, "SomeLength\n");
+	strcat(header, "Content-Type: text/html\n");
+	strcat(header, "Connection: ");
+	strcat(header, "My persistance\n");
+	fprintf(stdout, "HEADER:\n %s", header);
+
+
+	// Send data back to client
+	size_t size = strlen(header);
+	send(clientFd, header, size, 0);
+	fflush
+}
+/*
+void log(){
+
+}
+*/
 /*
 	HELPER FUNCTIONS GO HERE
 */
