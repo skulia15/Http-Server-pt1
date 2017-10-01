1bcb188c77a7fd87b635a3955f8f55dc952ff8a0
Skuli Arnarsson <skulia15@ru.is>
2017-10-01 15:26:34 +0000

    Sunday end, head is broken

--- src/httpd.c
+++ src/httpd.c
@@ -18,7 +18,8 @@
 #define DATA_SIZE 512
 #define MAX_CLIENTS 20
 #define HEADER_SIZE 512
-#define HOST_URL "http://localhost:"
+#define HOST_URL "http://localhost"
+#define DATE_TIME_SIZE 16
 
 // TODO
 /* PSEUDOCODE PLANNING
@@ -30,46 +31,69 @@
 	Test server in seperate teminal with command: curl -X (GET/POST/HEAD) localhost:59442
 	Test with data: curl -H "Content-Type: application/json" -X POST -d '{"username": "KYS"}' localhost:59442
 	scp /home/skulipardus/Háskólinn\ í\ Reykjavík/Tölvusamskipti/Http-Server-pt1/src/httpd.c  skulia15@skel.ru.is:tsam/pa2/src/httpd.c
+	Testing Post: curl -H "Content-Type: application/json" -X POST -d '{"username": "KYS", "data": "someData", "Connection" : "Connect plz"}' localhost:59442
 
-	validate args X
-	Find port number from args X
-	Connect to client X
+
+	validate args DONE
+	Find port number from args DONE
+	Connect to client DONE
 	Connect to multiple clients in parallel
-	Bind a TCP socket X
-	Receive message request from client X
+	Bind a TCP socket DONE
+	Receive message request from client DONE
 	Check cookie?
-	Parse the header
+	Parse the header?
 	Check for persistence
+		
 		if HTTP/1.0 -> not persistent
-	Check Operation (GET / POST/ HEAD)
-	Make Get
+		IF HTTP/1.1 -> persistent
+		if keep-alive != NULL -> persistent
+	Handle connection based on persistence
+	Handle timeout, 30 sec
+	Check Operation (GET / POST/ HEAD) DONE
+	Error response if method not supported
+	Make Get done??
 	Make Post
 	Make Head
 	Send Response
-	Send cookie
+	check if client gets response
 	Free Memory
+
 	Close the connection
+	FIX ALL WARNINGS
+	FIX ALL MEMORY LEAKS
+	CHECK IF MAKEFILE IS CORRECT - Compiles w/o warnings with flags given in description
+	Readme should give directions on how to test the program
+	(3 points) Make sure that your implementation does not crash, that memory is managed
+	correctly, and does not contain any obvious security issues. Try to keep track of what comes
+	from the clients, which may be malicious, and deallocate what you do not need early. Zero
+	out every buffer before use, or allocate the buffer using g_new0().
 
 	For each request print a single line to a log file that conforms to the format:
 		timestamp : <client ip>:<client port> <request method>
-		<requested URL> : <response code>
+		<requested URL> : <response code> DONE
 
 
 */
 /* Function definitions go here */
-void doMethod(int clientFd, gchar *methodType, gchar *protocol, struct sockaddr_in *clientAddress);
-void doGet(int clientFd, gchar *protocol, struct sockaddr_in *clientAddress);
-void doPost(int clientFd);
-void doHead(int clientFd, gchar *protocol);
+void doMethod(int clientFd, gchar *methodType, gchar *protocol, 
+	struct sockaddr_in *clientAddress, gchar *page, char* portNo, gchar* data);
+void doGet(int clientFd, gchar *protocol, struct sockaddr_in *clientAddress, gchar *page, char* portNo);
+void doPost(int clientFd, gchar *protocol, struct sockaddr_in *clientAddress, gchar *page, char* portNo, gchar* data);
+void doHead(int clientFd, gchar *protocol, struct sockaddr_in *clientAddress, gchar *page, char* portNo);
 void makeHeader(gchar *protocol, char *header, size_t contentLen);
-void log(struct sockaddr_in *clientAddress, gchar *methodType, int portNo);
-char* getIPAddress(struct sockaddr_in *clientAddress);
+void makeBody(struct sockaddr_in *clientAddress, gchar *page, char* portNo, char* html, gchar* data);
+void makeLogFile(struct sockaddr_in *clientAddress, gchar *methodType, int portNo);
+char *getIPAddress(struct sockaddr_in *clientAddress);
+void getTime(char* timeStamp);
+size_t my_strftime(char *s, size_t max, const char *fmt,const struct tm *tm);
+
 
 int main(int argc, char *argv[])
 {
 	int serverFd;
 	int clientFd;
 	int portNo;
+	char* portNoString;
 	struct sockaddr_in server, client;
 	char message[MESSAGE_SIZE];
 
@@ -84,7 +108,8 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		portNo = atoi(argv[1]);
+		portNo = strtol(argv[1], NULL, 10);
+		portNoString = argv[1];
 	}
 
 	// Create and bind a TCP socket
@@ -113,7 +138,8 @@ int main(int argc, char *argv[])
 	//timeout.tv_sec = 30;
 
 	// Listen for connections, max 128 connections (128 because it's good practice?)
-	if (listen(serverFd, 128) == -1)
+	// For only 1 connection at a time use 1
+	if (listen(serverFd, 1) == -1)
 	{
 		printf("Failed listening for connections. %s\n", strerror(errno));
 		close(serverFd);
@@ -129,7 +155,6 @@ int main(int argc, char *argv[])
 		{
 			printf("Failed to accept connection. %s\n", strerror(errno));
 		}
-		fprintf(stdout, "Client has connected\n\n");
 
 		ssize_t n = recvfrom(clientFd, message, sizeof(message) - 1, 0, (struct sockaddr *)&client, &len);
 		// Failed to receive from socket
@@ -158,11 +183,26 @@ int main(int argc, char *argv[])
 
 		// Split the string by newline and put them in an array
 		gchar **splitString = g_strsplit(message, "\r\n", countLines);
-		gchar **firstLine = g_strsplit(splitString[0], " / ", sizeof(splitString[0]));
+		gchar **firstLine = g_strsplit(splitString[0], " ", sizeof(splitString[0]));
 		// Find the method type (GET/POST/HEAD)
 		gchar *methodType = firstLine[0];
+		// Find the requested page in the request
+		gchar *page = firstLine[1];
 		// Find the protocol (HTTP.1.1)
-		gchar *protocol = firstLine[1];
+		gchar *protocol = firstLine[2];
+
+		// Data is found in request when we have seen 2 "\r\n" in a row.
+		gchar **headerDataSplit = g_strsplit(message, "\r\n\r\n", countLines);
+		gchar *data = headerDataSplit[1];
+		if(data != NULL){
+			printf("Data in Post Request: %s\n", data);
+			fflush(stdout);
+		}
+		else{
+			printf("No data found\n");
+			fflush(stdout);
+		}
+
 		// Find the value of the headers of the request
 		/*
 		gchar** headerFields;
@@ -174,8 +214,10 @@ int main(int argc, char *argv[])
 		}
 		*/
 		struct sockaddr_in *clientAddress = (struct sockaddr_in *)&client;
-		log(clientAddress, methodType, portNo);
-		doMethod(clientFd, methodType, protocol, clientAddress);
+		makeLogFile(clientAddress, methodType, portNo);
+		doMethod(clientFd, methodType, protocol, clientAddress, page, portNoString, data);
+		g_strfreev(splitString);
+		g_strfreev(firstLine);
 
 		// Check for persistence
 		/*
@@ -189,54 +231,42 @@ int main(int argc, char *argv[])
 		*/
 	}
 	close(clientFd);
-	close(serverFd);
+	//close(serverFd);
+	shutdown(clientFd, SHUT_RDWR);
 	return 0;
 }
-void doMethod(int clientFd, gchar *methodType, gchar *protocol, struct sockaddr_in *clientAddress)
+void doMethod(int clientFd, gchar *methodType, gchar *protocol,
+	 struct sockaddr_in *clientAddress, gchar *page, char* portNo, gchar* data)
 {
 	// Do method depending on the mode
 	if (g_strcmp0(methodType, "GET") == 0)
 	{
 		// Do get
-		doGet(clientFd, protocol, clientAddress);
+		doGet(clientFd, protocol, clientAddress, page, portNo);
 	}
 	else if (g_strcmp0(methodType, "POST") == 0)
 	{
 		// Do Post
-		doPost(clientFd);
+		doPost(clientFd, protocol, clientAddress, page, portNo, data);
 	}
 	else if (g_strcmp0(methodType, "HEAD") == 0)
 	{
 		// Do Head
-		doHead(clientFd, protocol);
+		doHead(clientFd, protocol, clientAddress, page, portNo);
 	}
 }
 
-void doGet(int clientFd, gchar *protocol, struct sockaddr_in *clientAddress)
+void doGet(int clientFd, gchar *protocol, struct sockaddr_in *clientAddress, gchar *page, char* portNo)
 {
 	// The HTML for the page
 	char html[DATA_SIZE];
-	memset(html, 0, sizeof(html));
+	makeBody(clientAddress, page, portNo, html, NULL);
 
-	// Open of HTML
-	strcat(html, "<!DOCTYPE html><html><body><h1>");
 
-	char content[DATA_SIZE];
-	memset(content, 0, sizeof(content));
-	
-	strcat(content, getIPAddress(clientAddress));
-
-	// Add content to HTML
-	strcat(html, content);
-
-	// Close HTML
-	strcat(html, "</h1></body></html>");
-	
-	
 	// Create the header
 	char header[HEADER_SIZE];
-	fflush(stdout);
 	makeHeader(protocol, header, strlen(html));
+
 	char response[MESSAGE_SIZE];
 	memset(response, 0, sizeof(response));
 
@@ -248,31 +278,47 @@ void doGet(int clientFd, gchar *protocol, struct sockaddr_in *clientAddress)
 	// Send data back to client
 	send(clientFd, response, sizeof(response), 0);
 }
-void doPost(int clientFd)
+void doPost(int clientFd, gchar *protocol, struct sockaddr_in *clientAddress, gchar *page, char* portNo, gchar* data)
 {
-	// Send data back to client
-	size_t size = strlen("Post Received\n");
-	send(clientFd, "Post Received\n", size, 0);
-}
-void doHead(int clientFd, gchar *protocol)
-{
+	// The HTML for the page
+	char html[DATA_SIZE];
+	makeBody(clientAddress, page, portNo, html, data);
+
 	// Create the header
 	char header[HEADER_SIZE];
-	makeHeader(protocol, header, 0);
+	makeHeader(protocol, header, strlen(html));
+
+	char response[MESSAGE_SIZE];
+	memset(response, 0, sizeof(response));
+
+	strcat(response, header);
+	strcat(response, html);
+
+	fprintf(stdout, "Response: \n%s", response);
+	fflush(stdout);
+	// Send data back to client
+	send(clientFd, response, sizeof(response), 0);
+}
+void doHead(int clientFd, gchar *protocol, struct sockaddr_in *clientAddress, gchar *page, char* portNo)
+{
+	
+	char html[DATA_SIZE];
+	makeBody(clientAddress, page, portNo, html, NULL);
+
+
+	// Create the header
+	char header[HEADER_SIZE];
+	makeHeader(protocol, header, strlen(html));
 
 	// Send data back to client
-	size_t size = strlen(header);
+	size_t size = sizeof(header);
 	send(clientFd, header, size, 0);
 }
 
 void makeHeader(gchar *protocol, char *header, size_t contentLen)
 {
-	// https://stackoverflow.com/questions/1442116/how-to-get-date-and-time-value-in-c-program/30759067#30759067
-	// Configure time to the current time
-	time_t t = time(NULL);
-	struct tm *tm = localtime(&t);
-	char timeStamp[64];
-	strftime(timeStamp, sizeof(timeStamp), "%c", tm);
+	char timeStamp[DATE_TIME_SIZE];
+	getTime(timeStamp);
 
 	// https://stackoverflow.com/questions/20685080/convert-size-t-to-string
 	char contentLenStr[sizeof(contentLen)];
@@ -282,29 +328,57 @@ void makeHeader(gchar *protocol, char *header, size_t contentLen)
 		persistance = false;
 	}*/
 
-	fflush(stdout);
 	memset(header, 0, HEADER_SIZE * sizeof(char));
 	// Fill header with values
 	strcat(header, protocol);
-	strcat(header, " 200 OK\n");
+	strcat(header, " 200 OK\r\n");
 	strcat(header, "Date: ");
 	strcat(header, timeStamp);
-	strcat(header, "\n");
+	strcat(header, "\r\n");
 	strcat(header, "Server: ");
-	strcat(header, "Great server name /v1.1/\n");
+	strcat(header, "Great server name /v1.1/\r\n");
 	strcat(header, "Content-Length: ");
 	strcat(header, contentLenStr);
-	strcat(header, "\n");
+	strcat(header, "\r\n");
 	strcat(header, "Content-Type: text/html\n");
 	strcat(header, "Connection: ");
-	strcat(header, " Closed\n");
+	strcat(header, " Closed\r\n");
+	// Do indicate that the header field is done
 	strcat(header, "\r\n");
+
 	/*if(persistance == false){
 		strcat(header, "Closed\n");
 	}*/
 }
 
-void log(struct sockaddr_in *clientAddress, gchar *methodType, int portNo)
+void makeBody(struct sockaddr_in *clientAddress, gchar *page, char* portNo, char* html, gchar* data)
+{
+	memset(html, 0, DATA_SIZE);
+	
+	// Open of HTML
+	strcat(html, "<!DOCTYPE html><html><body><h1>");
+
+	// Add this to the html URL/RequestedPage ClientIP:Portnumber
+	char content[DATA_SIZE];
+	memset(content, 0, sizeof(content));
+	strcat(content, HOST_URL);
+	strcat(content, page);
+	strcat(content, " ");
+	strcat(content, getIPAddress(clientAddress));
+	strcat(content, ":");
+	strcat(content, portNo);
+
+	// Add content to HTML
+	strcat(html, content);
+	if(data != NULL){
+		strcat(html, data);
+	}
+
+	// Close HTML
+	strcat(html, "</h1></body></html>");
+}
+
+void makeLogFile(struct sockaddr_in *clientAddress, gchar *methodType, int portNo)
 {
 	FILE *logFile;
 	/*
@@ -319,11 +393,9 @@ void log(struct sockaddr_in *clientAddress, gchar *methodType, int portNo)
 		printf("Failed to create logfile %s\n", strerror(errno));
 	}
 
-	// Get current time !!!Watch out for duplicate code and reference to stackoverflow see makeheader()
-	time_t t = time(NULL);
-	struct tm *tm = localtime(&t);
-	char timeStamp[64];
-	strftime(timeStamp, sizeof(timeStamp), "%c", tm);
+	// Get current time
+	char timeStamp[DATE_TIME_SIZE];
+	getTime(timeStamp);
 
 	char IPClientStr[INET_ADDRSTRLEN];
 	memset(IPClientStr, 0, sizeof(IPClientStr));
@@ -339,7 +411,7 @@ void log(struct sockaddr_in *clientAddress, gchar *methodType, int portNo)
 	// Print request method
 	fprintf(logFile, "%s ", methodType);
 	// Print requested URL
-	fprintf(logFile, "%s%d : ", HOST_URL, portNo);
+	fprintf(logFile, "%s:%d : ", HOST_URL, portNo);
 	// Print response code
 	// TODO: get actual response code? priority: low
 	fprintf(logFile, "200 OK\n");
@@ -347,13 +419,27 @@ void log(struct sockaddr_in *clientAddress, gchar *methodType, int portNo)
 	fclose(logFile);
 }
 
-char* getIPAddress(struct sockaddr_in *clientAddress) 
+char *getIPAddress(struct sockaddr_in *clientAddress)
 {
 	// https://stackoverflow.com/questions/3060950/how-to-get-ip-address-from-sock-structure-in-c
 	// Get the ip address of a client
 	struct in_addr ipAddr = clientAddress->sin_addr;
 	char IPClientStr[INET_ADDRSTRLEN];
 	inet_ntop(AF_INET, &ipAddr, IPClientStr, INET_ADDRSTRLEN);
-	
+
 	return IPClientStr;
 }
+
+void getTime(char* timeStamp){
+	// https://stackoverflow.com/questions/1442116/how-to-get-date-and-time-value-in-c-program/30759067#30759067
+	// Configure time to the current time
+	time_t t = time(NULL);
+	struct tm *tm = localtime(&t);
+	strftime(timeStamp, sizeof(timeStamp), "%c", tm);
+}
+
+// Fix for a bug in gcc that complains that %c yields only last 2 digits of year in some locales.
+// size_t my_strftime(char *s, size_t max, const char *fmt,const struct tm *tm)
+// {
+// 	return strftime(s, max, fmt, tm);
+// }
